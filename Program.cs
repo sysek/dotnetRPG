b9158e5c3b249fa9fc0af13feb89f282f766d1aa
sysek <mateusz.mielczarek@outlook.com>
2021-02-16 20:28:51 +0100

    Add Player class base

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace dotnetRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player();
+
+            Console.WriteLine("This is player with no name");
+            Console.WriteLine("Health: \t{0}", player.Health);
+            Console.WriteLine("Mana: \t\t{0}", player.Mana);
+            Console.WriteLine("Gold: \t\t{0}", player.Gold);
         }
     }
 }
